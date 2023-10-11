33288d15405d8a81e4ea173d0828e76c54fbdf1c
idea&steps <202240100820@jxnu.edu.cn>
2023-10-11 08:59:45 +0000

    update

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -3,9 +3,8 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+// comment: Constants types must also always be annotated.
+const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
